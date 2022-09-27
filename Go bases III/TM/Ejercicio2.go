6ad6b451c37118389ab0cf9ac7da16453e0c8153
tinchourteaga-ml <martin.urteaga@mercadolibre.com>
2022-09-27 12:55:48 -0300

    Ejercicios TM

--- Go bases III/TM/Ejercicio2.go
+++ Go bases III/TM/Ejercicio2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,10 +12,13 @@ func main() {
 	filePath := fmt.Sprint(path, "/myFile.csv")
 	content, _ := os.ReadFile(filePath)
 	prodLists := strings.Split(string(content), "\n")
-
+	var sum float64
 	fmt.Printf("ID\t Precio Cantidad\n")
 	for _, prod := range prodLists[0:3] {
 		p := strings.Split(prod, ";")
+		price, _ := strconv.ParseFloat(p[1], 64)
+		sum += price
 		fmt.Printf("%s\t %s\t %s\n", p[0], p[1], p[2])
 	}
+	fmt.Printf("\t %.2f\t \n", sum)
 }
