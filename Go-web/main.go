d76d6fdee3924ee8067f1f2f79429953fe945347
tinchourteaga-ml <martin.urteaga@mercadolibre.com>
2022-10-03 12:43:01 -0300

    greetings and products catalog

--- Go-web/main.go
+++ Go-web/main.go
@@ -1,15 +1,57 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 
-func saludoHandler(ctx *gin.Context) {
-	ctx.JSON(299, gin.H{
-		"message": "Hola\n",
+	"github.com/gin-gonic/gin"
+)
+
+var P1 = Product{
+	Id:           1,
+	Name:         "racket",
+	Color:        "green",
+	Price:        6000,
+	Stock:        25,
+	Code:         "AB230F",
+	Published:    true,
+	CreationDate: "23/10/2021",
+}
+
+type ProductsCatalog struct {
+	Products []Product
+}
+
+type Product struct {
+	Id           int
+	Name         string
+	Color        string
+	Price        float64
+	Stock        int
+	Code         string
+	Published    bool
+	CreationDate string
+}
+
+func greetingsHandler(ctx *gin.Context) {
+	name := "Martin"
+	ctx.JSON(200, gin.H{
+		"message": "Hola " + name,
 	})
+}
+
+func getAllHandler(ctx *gin.Context) {
+	file, _ := ioutil.ReadFile("products.json")
+	catalog := ProductsCatalog{}
+	json.Unmarshal([]byte(file), &catalog)
+	fmt.Println(catalog)
+	ctx.JSON(200, catalog)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/saludo", saludoHandler)
+	router.GET("/greetings", greetingsHandler)
+	router.GET("/products", getAllHandler)
 	router.Run()
 }
