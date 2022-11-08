11559d1d67a5750ba0e6b0e8e4d828ec3be7e9b3
tinchourteaga-ml <martin.urteaga@mercadolibre.com>
2022-11-08 13:28:38 -0300

    main and new init

--- Databases/Go-storage/Go-storage-I/pkg/store/db.go
+++ Databases/Go-storage/Go-storage-I/pkg/store/db.go
@@ -3,6 +3,11 @@ package store
 import (
 	"database/sql"
 	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var StorageDB *sql.DB
@@ -21,4 +26,30 @@ func init() {
 	}
 
 	log.Println("database configured")
+}
+
+// Otra forma de inicializar la conexion
+
+func connectToDatabse() (engine *gin.Engine, db *sql.DB) {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	configDB := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: os.Getenv("DBNAME"),
+	}
+
+	db, err = sql.Open("mysql", configDB.FormatDSN())
+
+	if err != nil {
+		panic(err)
+	}
+
+	return engine, db
 }
