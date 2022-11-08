814363fab78c1a4d6ffc56d10fd6bebf7d10ccb6
tinchourteaga-ml <martin.urteaga@mercadolibre.com>
2022-11-08 12:46:44 -0300

    store method

--- Databases/Go-storage/Go-storage-I/internal/products/repository.go
+++ Databases/Go-storage/Go-storage-I/internal/products/repository.go
@@ -1,1 +1,54 @@
 package products
+
+import (
+	"log"
+
+	"github.com/tinchourteaga-ml/backpack-bcgow6-martin-urteaga/Databases/Go-storage/Go-storage-I/internal/products/domain"
+	"github.com/tinchourteaga-ml/backpack-bcgow6-martin-urteaga/Databases/Go-storage/Go-storage-I/pkg/store"
+)
+
+type Repository interface {
+	Store(product domain.Product) (domain.Product, error)
+	GetByName(productName string) domain.Product
+}
+
+type repository struct{}
+
+var (
+	db           = store.StorageDB
+	storeProduct = "INSERT INTO products(name, qty, price) VALUES (?, ?, ?)"
+)
+
+func newRepository() Repository {
+	return &repository{}
+}
+
+func (repo *repository) Store(product domain.Product) (domain.Product, error) {
+	stmt, err := db.Prepare(storeProduct)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(product.Name, product.Qty, product.Price)
+
+	if err != nil {
+		return domain.Product{}, err
+	}
+
+	insertedID, err := result.LastInsertId()
+
+	if err != nil {
+		return domain.Product{}, err
+	}
+
+	product.ID = int(insertedID)
+
+	return product, nil
+}
+
+func (repo *repository) GetByName(productName string) domain.Product {
+	return domain.Product{}
+}
